9273ff4c0dedfe5a5f6662057bb837e242037d64
Natasha Kerensikova <natacha@instinctive.eu>
2010-07-12 17:01:17 +0000

    lace: addition of command-line options to select renderer

--- lace.c
+++ lace.c
@@ -19,8 +19,10 @@
 #include "markdown.h"
 #include "renderers.h"
 
-#include <stdio.h>
 #include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define READ_UNIT 1024
@@ -33,19 +35,93 @@ extern size_t buffer_stat_alloc_bytes;
 
 
 
+/* usage • print the option list */
+void
+usage(FILE *out, const char *name) {
+	fprintf(out, "Usage: %s [-h | -x] [-d | -m | -n] [input-file]\n\n",
+	    name);
+	fprintf(out, "\t-d, --discount\n"
+	    "\t\tEnable some Discount extensions (image size specficiation,\n"
+	    "\t\tclass blocks and 'abbr:', 'class:', 'id:' and 'raw:'\n"
+	    "\t\tpseudo-protocols)\n"
+	    "\t-H, --html\n"
+	    "\t\tOutput HTML-style self-closing tags (e.g. <br>)\n"
+	    "\t-h, --help\n"
+	    "\t\tDisplay this help text and exit without further processing\n"
+	    "\t-m, --markdown\n"
+	    "\t\tDisable all extensions and use strict markdown syntax\n"
+	    "\t-n, --natext\n"
+	    "\t\tEnable support Discount extensions and Natasha's own\n"
+	    "\t\textensions (id header attribute, class paragraph attribute,\n"
+	    "\t\t'ins' and 'del' elements, and plain span elements)\n"
+	    "\t-x, --xhtml\n"
+	    "\t\tOutput XHTML-style self-closing tags (e.g. <br />)\n"); }
+
+
+
 /* main • main function, interfacing STDIO with the parser */
 int
 main(int argc, char **argv) {
 	struct buf *ib, *ob;
 	size_t ret;
 	FILE *in = stdin;
+	const struct mkd_renderer *hrndr, *xrndr;
+	const struct mkd_renderer **prndr;
+	int ch, argerr, help;
+	struct option longopts[] = {
+	    { "discount",	no_argument,	0,	'd' },
+	    { "html",		no_argument,	0,	'H' },
+	    { "help",		no_argument,	0,	'h' },
+	    { "markdown",	no_argument,	0,	'm' },
+	    { "natext",		no_argument,	0,	'n' },
+	    { "xhtml",		no_argument,	0,	'x' },
+	    { 0,		0,		0,	0 } };
+
+	/* default options: strict markdown input, HTML output */
+	hrndr = &mkd_html;
+	xrndr = &mkd_xhtml;
+	prndr = &hrndr;
+
+	/* argument parsing */
+	argerr = help = 0;
+	while (!argerr &&
+	    (ch = getopt_long(argc, argv, "dHhmnx", longopts, 0)) != -1)
+		switch (ch) {
+		    case 'd': /* discount extension */
+			hrndr = &discount_html;
+			xrndr = &discount_xhtml;
+			break;
+		    case 'H': /* HTML output */
+			prndr = &hrndr;
+			break;
+		    case 'h': /* display help */
+			argerr = help = 1;
+			break;
+		    case 'm': /* strict markdown */
+			hrndr = &mkd_html;
+			xrndr = &mkd_xhtml;
+			break;
+		    case 'n': /* Discount + Natasha's extensions */
+			hrndr = &nat_html;
+			xrndr = &nat_xhtml;
+			break;
+		    case 'x': /* XHTML output */
+			prndr = &xrndr;
+			break;
+		    default:
+			argerr = 1; }
+	if (argerr) {
+		usage(help ? stdout : stderr, argv[0]);
+		return help ? EXIT_SUCCESS : EXIT_FAILURE; }
+	argc -= optind;
+	argv += optind;
 
 	/* opening the file if given from the command line */
-	if (argc > 1) {
-		in = fopen(argv[1], "r");
+	if (argc > 0) {
+		in = fopen(argv[0], "r");
 		if (!in) {
 			fprintf(stderr,"Unable to open input file \"%s\": %s\n",
-				argv[1], strerror(errno));
+				argv[0], strerror(errno));
 			return 1; } }
 
 	/* reading everything */
@@ -59,7 +135,7 @@ main(int argc, char **argv) {
 
 	/* performing markdown parsing */
 	ob = bufnew(OUTPUT_UNIT);
-	markdown(ob, ib, &mkd_xhtml);
+	markdown(ob, ib, *prndr);
 
 	/* writing the result to stdout */
 	fwrite(ob->data, 1, ob->size, stdout);
