08dfa75e56ec221cd30a744366692ac58e3ef702
Natasha Kerensikova <natacha@instinctive.eu>
2011-04-28 16:18:29 +0000

    buffer: only call `bufgrow()` internally when it's needed

--- buffer.c
+++ buffer.c
@@ -173,7 +173,7 @@ void
 bufnullterm(struct buf *buf) {
 	if (!buf || !buf->unit) return;
 	if (buf->size < buf->asize && buf->data[buf->size] == 0) return;
-	if (bufgrow(buf, buf->size + 1))
+	if (buf->size + 1 <= buf->asize || bufgrow(buf, buf->size + 1))
 		buf->data[buf->size] = 0; }
 
 
@@ -190,7 +190,9 @@ bufprintf(struct buf *buf, const char *fmt, ...) {
 /* bufput • appends raw data to a buffer */
 void
 bufput(struct buf *buf, const void *data, size_t len) {
-	if (!buf || !bufgrow(buf, buf->size + len)) return;
+	if (!buf) return;
+	if (buf->size + len > buf->asize && !bufgrow(buf, buf->size + len))
+		return;
 	memcpy(buf->data + buf->size, data, len);
 	buf->size += len; }
 
@@ -204,7 +206,9 @@ bufputs(struct buf *buf, const char *str) {
 /* bufputc • appends a single char to a buffer */
 void
 bufputc(struct buf *buf, char c) {
-	if (!buf || !bufgrow(buf, buf->size + 1)) return;
+	if (!buf) return;
+	if (buf->size + 1 > buf->asize && !bufgrow(buf, buf->size + 1))
+		return;
 	buf->data[buf->size] = c;
 	buf->size += 1; }
 
@@ -285,7 +289,9 @@ vbufprintf(struct buf *buf, const char *fmt, va_list ap) {
 	va_copy(ap_save, ap);
 	n = vsnprintf(buf->data + buf->size, buf->asize - buf->size, fmt, ap);
 	if (n >= buf->asize - buf->size) {
-		if (!bufgrow (buf, buf->size + n + 1)) return;
+		if (buf->size + n + 1 > buf->asize
+		&& !bufgrow (buf, buf->size + n + 1))
+			return;
 		n = vsnprintf (buf->data + buf->size,
 				buf->asize - buf->size, fmt, ap_save); }
 	va_end(ap_save);
